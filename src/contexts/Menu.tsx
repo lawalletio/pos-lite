59610b30e2af5bcb7355be4517dd0ee752239fd1
Agustin Kassis <webmaster@masize.com>
2023-08-22 20:39:11 -0300

    updated Menu

--- src/contexts/Menu.tsx
+++ src/contexts/Menu.tsx
@@ -9,41 +9,34 @@ import {
 
 // Utils
 import { fetchMenuItems, calcTotal } from "~/lib/utils";
-import { utils } from "lnurl-pay";
-import { requestInvoice } from "lnurl-pay";
-import {
-  generatePrivateKey,
-  getPublicKey,
-  getSignature,
-  getEventHash,
-} from "nostr-tools";
+import { generatePrivateKey, getPublicKey } from "nostr-tools";
+import axios from "axios";
 
 // Types
 import type { Dispatch, SetStateAction } from "react";
 import type { IMenuItem } from "~/types/menu";
-import type {
-  LnUrlRequestInvoiceArgs,
-  LnUrlRequestInvoiceResponse,
-} from "lnurl-pay/dist/types/types";
-import type { UnsignedEvent, Event } from "nostr-tools";
+import { useLN } from "./LN";
+import { useNostr } from "./Nostr";
 
 // Interface
 export interface IMenuContext {
   total: number;
+  totalSats: number;
   menuItems: IMenuItem[];
-  invoice?: LnUrlRequestInvoiceResponse;
+  invoice?: string;
   setMenuItems: Dispatch<SetStateAction<IMenuItem[]>>;
-  checkOut: () => Promise<LnUrlRequestInvoiceResponse>;
+  checkOut: () => Promise<string>;
 }
 
 // Context
 export const MenuContext = createContext<IMenuContext>({
   total: 0,
+  totalSats: 0,
   menuItems: [],
   setMenuItems: function (_value: SetStateAction<IMenuItem[]>): void {
     throw new Error("Function not implemented.");
   },
-  checkOut: function (): Promise<LnUrlRequestInvoiceResponse> {
+  checkOut: function (): Promise<string> {
     throw new Error("Function not implemented.");
   },
 });
@@ -53,56 +46,34 @@ interface IMenuProviderProps {
   children: React.ReactNode;
 }
 
-const DESTINATION_LNURL = process.env.NEXT_PUBLIC_DESTINATION!;
-const NOSTR_RELAY = process.env.NEXT_PUBLIC_NOSTR_RELAY!;
+const SAT_ARS_RATE = 0.18;
 
 export const MenuProvider = ({ children }: IMenuProviderProps) => {
   const [menuItems, setMenuItems] = useState<IMenuItem[]>([]);
   const [total, setTotal] = useState<number>(0);
-  const [invoice, setInvoice] = useState<LnUrlRequestInvoiceResponse>();
+  const [totalSats, setTotalSats] = useState<number>(0);
+  const [invoice, setInvoice] = useState<string>();
+
+  const { callbackUrl, destination } = useLN();
+  const {} = useNostr();
 
   // Checkout function
-  const checkOut =
-    useCallback(async (): Promise<LnUrlRequestInvoiceResponse> => {
-      const args: LnUrlRequestInvoiceArgs = {
-        lnUrlOrAddress: DESTINATION_LNURL,
-        tokens: utils.toSats(total),
-        comment: "Algo prueba",
-      };
+  const checkOut = useCallback(async (): Promise<string> => {
+    const amountMillisats = totalSats * 1000;
 
-      const receipientPubkey = "asdasd";
-      const relays = [NOSTR_RELAY];
+    const encodedEvent = encodeURI(JSON.stringify(event));
 
-      // This POS
-      const senderPrivkey = generatePrivateKey();
-      const senderPubkey = getPublicKey(senderPrivkey);
+    // fetch json using axios
+    const response = await axios.get(
+      `${callbackUrl}?amount=${amountMillisats}&nostr=${encodedEvent}&lnurl=${destination}`
+    );
 
-      const unsignedEvent: UnsignedEvent = {
-        kind: 9734,
-        content: "",
-        pubkey: senderPubkey,
-        created_at: Math.round(Date.now() / 1000),
-        tags: [
-          ["relays", ...relays],
-          ["amount", total.toString()],
-          ["lnurl", DESTINATION_LNURL],
-          ["p", receipientPubkey],
-        ] as string[][],
-      };
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const invoice = response.data.pr as string;
+    setInvoice(invoice);
 
-      const event: Event = {
-        id: getEventHash(unsignedEvent),
-        sig: getSignature(unsignedEvent, senderPrivkey),
-        ...unsignedEvent,
-      };
-
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const encodedEvent = encodeURI(JSON.stringify(event));
-
-      const invoice: LnUrlRequestInvoiceResponse = await requestInvoice(args);
-      setInvoice(invoice);
-      return invoice;
-    }, [total]);
+    return invoice;
+  }, [totalSats]);
 
   // Fetch menu items
   useEffect(() => {
@@ -111,12 +82,14 @@ export const MenuProvider = ({ children }: IMenuProviderProps) => {
 
   // Calculate total on menu change
   useEffect(() => {
-    setTotal(calcTotal(menuItems));
+    const _total = calcTotal(menuItems);
+    setTotal(_total);
+    setTotalSats(_total * SAT_ARS_RATE);
   }, [menuItems]);
 
   return (
     <MenuContext.Provider
-      value={{ total, invoice, menuItems, setMenuItems, checkOut }}
+      value={{ total, totalSats, invoice, menuItems, setMenuItems, checkOut }}
     >
       {children}
     </MenuContext.Provider>
