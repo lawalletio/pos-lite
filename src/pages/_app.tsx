4fa45e776b1185bee29961e3507523772c7dfbb0
Agustin Kassis <webmaster@masize.com>
2023-08-22 20:39:30 -0300

    applied Providers for app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { LNProvider } from "~/contexts/LN";
 import { MenuProvider } from "~/contexts/Menu";
+import { NostrProvider } from "~/contexts/Nostr";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <MenuProvider>
-      <Component {...pageProps} />
-    </MenuProvider>
+    <LNProvider>
+      <NostrProvider>
+        <MenuProvider>
+          <Component {...pageProps} />
+        </MenuProvider>
+      </NostrProvider>
+    </LNProvider>
   );
 };
 
